fd016bcde68b702c3f3a8e8ac08100a70bccb38d
Jonathan Orrego Montoya <jonathan.orrego@ucp.edu.co>
2025-10-22 23:18:11 -0500

    Nuevo componente modal, función, props, etc

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
-import { ref } from "vue";
+import { ref, reactive } from "vue";
 import Presupuesto from "./components/Presupuesto.vue";
 import ControlPresupuesto from "./components/ControlPresupuesto.vue";
 import iconoNuevoGasto from "./assets/img/nuevo-gasto.svg";
+import Modal from "./components/Modal.vue";
+
+const modal = reactive({
+  mostrar: false,
+  animar: false,
+});
 
 const presupuesto = ref(0);
 const disponible = ref(0);
@@ -10,6 +16,16 @@ const disponible = ref(0);
 const definirPresupuesto = (cantidad) => {
   presupuesto.value = cantidad;
   disponible.value = cantidad;
+};
+
+const mostrarModal = () => {
+  modal.mostrar = true;
+  modal.animar = true;
+};
+
+const ocultarModal = () => {
+  modal.animar = false;
+  modal.mostrar = false;
 };
 </script>
 
@@ -32,8 +48,14 @@ const definirPresupuesto = (cantidad) => {
 
     <main v-if="presupuesto > 0">
       <div class="crear-gasto">
-        <img :src="iconoNuevoGasto" alt="icono nuevo gasto" />
+        <img
+          :src="iconoNuevoGasto"
+          alt="icono nuevo gasto"
+          @click="mostrarModal"
+        />
       </div>
+
+      <Modal v-if="modal.mostrar" @ocultar-modal="ocultarModal" />
     </main>
   </div>
 </template>
